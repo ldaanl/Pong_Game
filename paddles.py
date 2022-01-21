5ca3b5bc4e8359a1a64b5a269569231e2c12363f
Daan <giltchutoy81@gmail.com>
2022-01-21 11:13:19 -0600

    Modificacion a la clase Paddles

--- paddles.py
+++ paddles.py
@@ -1,24 +1,22 @@
-from turtle import Turtle, color
+from turtle import Turtle
 
 class Paddles(Turtle):
 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.paddle_init()
-    
-    def paddle_init(self):
-        self.paddle = Turtle("square")
-        self.paddle.color("white")
-        self.paddle.turtlesize(5,1)
-        self.paddle.pu()
-        self.paddle.goto(self.x, self.y)
+    def __init__(self,x,y):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.turtlesize(5,1)
+        self.pu()
+        self.goto(x,y)
     
     def move_up(self):
-        y = self.paddle.ycor() + 20
-        self.paddle.goto(self.paddle.xcor(), y)
+        if self.ycor() < 250:
+            y = self.ycor() + 40
+            self.goto(self.xcor(), y)
     
     def move_down(self):
-        y = self.paddle.ycor() - 20
-        self.paddle.goto(self.paddle.xcor(), y)
-    
+        if self.ycor() > -250:
+            y = self.ycor() - 40
+            self.goto(self.xcor(), y)
+        
